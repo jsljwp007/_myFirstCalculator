a74234df95e53ba449b26ffe61064430515f97ef
Francis Wang <51566243+jsljwp007@users.noreply.github.com>
2022-01-18 10:43:15 +0800

    Initial commit

---
+++ src/main/java/com/example/demo6/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo6;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onWelcomeClick(){welcomeText.setText("你好");}
+}
